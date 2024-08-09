449898dbc2cd358fdad29d2da3c3a87c153a1b5b
adityaJ <adityajjagtap.0001@gmail.com>
2024-08-09 13:17:27 +0530

    again

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mcqgenrator@',  # Remove the '@' symbol as it’s unconventional
-    version='0.1.0',
+    name='mcqgenrator',  # Simple, valid package name
+    version='0.1.0',     # Correct version format
     author='aditya',
     author_email='adityajjagtap.0001@gmail.com',
     install_requires=[
